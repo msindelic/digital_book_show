8f146530c7a6d9afebaf9d609103eb555a5eb2ba
ms84 <msindelicm@gmail.com>
2026-05-29 16:25:18 +0200

    added deployment :rocket:

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/msindelic',
+  base: '/digital_book_show/',
   server: {
     port: 5173,
     open: true,
